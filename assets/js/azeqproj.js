b761884bfd59971e7567d52384a547ed38b7c2a8
Roger Allen <rallen@gmail.com>
2023-03-16 08:21:25 -0700

    add proper pointer handling for mobile (i hope)
    used info from https://www.redblobgames.com/making-of/draggable/

--- assets/js/azeqproj.js
+++ assets/js/azeqproj.js
@@ -1,4 +1,4 @@
-if ( ! Detector.webgl ) Detector.addGetWebGLMessage({id:"placeholder"});
+if (!Detector.webgl) Detector.addGetWebGLMessage({ id: "placeholder" });
 
 var dim = 512;
 var scene = new THREE.Scene();
@@ -15,24 +15,26 @@ var curLongitude = 0.0;
 
 // http://www.khronos.org/message_boards/showthread.php/7170-How-to-include-shaders
 function getSourceSync(url) {
-  var req = new XMLHttpRequest();
-  req.open("GET", url, false);
-  req.send(null);
-  return (req.status == 200) ? req.responseText : null;
+    var req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
+    return (req.status == 200) ? req.responseText : null;
 };
 
 function deg2rad(deg) {
-    return deg*(Math.PI/180);
+    return deg * (Math.PI / 180);
 }
 
 function rad2deg(rad) {
-    return rad*(180/Math.PI);
+    return rad * (180 / Math.PI);
 }
 
 function handleMouseDown(event) {
+    if (event.button !== 0) return; // left button only
     mouseDown = true;
     lastMouseX = event.clientX;
     lastMouseY = event.clientY;
+    renderer.domElement.setPointerCapture(event.pointerId);
 }
 
 function handleMouseUp(event) {
@@ -40,7 +42,7 @@ function handleMouseUp(event) {
 }
 
 function handleMouseMove(event) {
-    if  (!mouseDown) {
+    if (!mouseDown) {
         return;
     }
     var newX = event.clientX;
@@ -52,8 +54,8 @@ function handleMouseMove(event) {
     //var cur_latitude = deg2rad(parseFloat(document.getElementById("cur_latitude").value));
     //var cur_longitude = deg2rad(parseFloat(document.getElementById("cur_longitude").value));
 
-    curLatitude += deltaY/100.0;
-    curLongitude -= deltaX/100.0;
+    curLatitude += deltaY / 100.0;
+    curLongitude -= deltaX / 100.0;
 
     //document.getElementById("cur_latitude").value = rad2deg(cur_latitude);
     //document.getElementById("cur_longitude").value = rad2deg(cur_longitude);
@@ -63,13 +65,17 @@ function handleMouseMove(event) {
 }
 
 function initCanvas() {
-    renderer.setSize(dim,dim);
+    renderer.setSize(dim, dim);
     div = document.getElementById("placeholder");
     div.appendChild(renderer.domElement);
 
-    renderer.domElement.onmousedown = handleMouseDown;
-    document.onmouseup = handleMouseUp;
-    document.onmousemove = handleMouseMove;
+    // https://www.redblobgames.com/making-of/draggable/
+    el = renderer.domElement;
+    el.addEventListener('pointerdown', handleMouseDown);
+    el.addEventListener('pointerup', handleMouseUp);
+    el.addEventListener('pointercancel', handleMouseUp);
+    el.addEventListener('pointermove', handleMouseMove)
+    el.addEventListener('touchstart', (e) => e.preventDefault());
 
     var worldTexture = THREE.ImageUtils.loadTexture('/assets/image/world1024x512.jpg');
     // without this, there is a line where the world wraps in the Pacific
@@ -78,17 +84,17 @@ function initCanvas() {
     worldTexture.wrapS = THREE.RepeatWrapping;
     worldTexture.wrapT = THREE.RepeatWrapping;
     uniforms = {
-        texture1: {type: "t", value: worldTexture},
-        phi1:     {type: "f", value: 0.0},
-        lambda0:  {type: "f", value: 0.0}
+        texture1: { type: "t", value: worldTexture },
+        phi1: { type: "f", value: 0.0 },
+        lambda0: { type: "f", value: 0.0 }
     };
     var vertShader = getSourceSync("/assets/js/azeqproj_vs.glsl");
     var fragShader = getSourceSync("/assets/js/azeqproj_fs.glsl");
 
     var mapMaterial = new THREE.ShaderMaterial({
-        side:           THREE.DoubleSide,
-        uniforms:       uniforms,
-        vertexShader:   vertShader,
+        side: THREE.DoubleSide,
+        uniforms: uniforms,
+        vertexShader: vertShader,
         fragmentShader: fragShader
     });
 
@@ -107,12 +113,12 @@ function initCanvas() {
     var map = new THREE.Mesh(mapGeometry, mapMaterial);
     scene.add(map);
 
-    var lineMaterial = new THREE.LineBasicMaterial({color: 0xffff00});
+    var lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
     var crossGeometry = new THREE.Geometry();
     var crossDelta = 0.02;
     crossGeometry.vertices.push(new THREE.Vector3(-crossDelta, 0, 0.1));
     crossGeometry.vertices.push(new THREE.Vector3(crossDelta, 0, 0.1));
-    crossGeometry.vertices.push(new THREE.Vector3(0,-crossDelta, 0.1));
+    crossGeometry.vertices.push(new THREE.Vector3(0, -crossDelta, 0.1));
     crossGeometry.vertices.push(new THREE.Vector3(0, crossDelta, 0.1));
     var cross = new THREE.Line(crossGeometry, lineMaterial, THREE.LinePieces);
     scene.add(cross);
@@ -120,8 +126,8 @@ function initCanvas() {
     // Equatorial Circumference of the earth = 40,075.017 km
     var COE = 40075.017;
     var circleStep = 5000;
-    for(var km = circleStep; km < COE; km += circleStep) {
-        var i = km/COE;
+    for (var km = circleStep; km < COE; km += circleStep) {
+        var i = km / COE;
         var circleGeometry = new THREE.CircleGeometry(i, 100);
         circleGeometry.vertices.shift(); // pop off the circle center
         circleGeometry.vertices.verticesNeedUpdate;
